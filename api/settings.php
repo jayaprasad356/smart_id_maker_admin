5744a71db7962fd5ba692ef5941572d34b1588b1
Akraja712 <ashokkumar7hitter@gmail.com>
2024-12-07 15:19:06 +0530

    settings.php

--- api/settings.php
+++ api/settings.php
@@ -11,16 +11,17 @@ include_once('../includes/crud.php');
 date_default_timezone_set('Asia/Kolkata');
 include_once('../includes/functions.php');
 $fn = new functions;
-$fn->monitorApi('settings');
+
 $db = new Database();
 $db->connect();
-$sql = "SELECT chat_support FROM `settings`";
+$sql = "SELECT * FROM `settings`";
 $db->sql($sql);
 $res = $db->getResult();
 $num = $db->numRows($res);
 if ($num >= 1) {
     $response['success'] = true;
     $response['message'] = "Settings listed Successfully";
+    $response['demo_video'] = "https://youtu.be/91C9epbUXks";
     $response['data'] = $res;
     print_r(json_encode($response));
 

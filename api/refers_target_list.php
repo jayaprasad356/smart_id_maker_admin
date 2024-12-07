762faf35d3fabf6c140ee995bbc5fd8075850310
Akraja712 <ashokkumar7hitter@gmail.com>
2024-12-07 11:16:28 +0530

    refers_target_list.php

--- api/refers_target_list.php
+++ api/refers_target_list.php
@@ -21,7 +21,8 @@ if (empty($_POST['user_id'])) {
 
 $user_id = $db->escapeString($_POST['user_id']);
 
-$sql = "SELECT * FROM users WHERE id = $user_id";
+// Fetch total_referrals from users table
+$sql = "SELECT total_referrals FROM users WHERE id = $user_id";
 $db->sql($sql);
 $user = $db->getResult();
 
@@ -32,6 +33,9 @@ if (empty($user)) {
     return false;
 }
 
+$total_referrals = (int)$user[0]['total_referrals'];
+
+// Fetch all refers_target
 $sql = "SELECT * FROM refers_target";
 $db->sql($sql);
 $res = $db->getResult();
@@ -39,25 +43,26 @@ $num = $db->numRows($res);
 
 if ($num >= 1) {
     foreach ($res as &$refer) {
-        $refer_id = $refer['id'];
-        $sql = "SELECT * FROM refer_counts WHERE user_id = $user_id AND refer_id = $refer_id";
-        $db->sql($sql);
-        $refer_count = $db->getResult();
+        $refer_id = (int)$refer['id'];
+        $refer['status'] = 0;
 
-        if (!empty($refer_count)) {
+        if (($total_referrals >= 5 && $total_referrals <= 9) && $refer_id == 1) {
             $refer['status'] = 1;
-        } else {
-            $refer['status'] = 0;
+        } elseif (($total_referrals >= 10 && $total_referrals <= 19) && in_array($refer_id, [1, 2])) {
+            $refer['status'] = 1;
+        } elseif (($total_referrals >= 20 && $total_referrals <= 29) && in_array($refer_id, [1, 2, 3])) {
+            $refer['status'] = 1;
+        } elseif ($total_referrals >= 30 && in_array($refer_id, [1, 2, 3, 4])) {
+            $refer['status'] = 1;
         }
     }
 
-    // Return the final data with statuses
     $response['success'] = true;
     $response['message'] = "Refers Target Listed Successfully";
     $response['data'] = $res;
     print_r(json_encode($response));
 } else {
-    // If no records found in refers_target
+
     $response['success'] = false;
     $response['message'] = "No Data Found";
     print_r(json_encode($response));

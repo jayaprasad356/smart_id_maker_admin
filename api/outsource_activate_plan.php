0ea912f3c19d9ada87ec8d7a656d1fc0e0773828
Akraja712 <ashokkumar7hitter@gmail.com>
2025-01-19 13:31:36 +0530

    outsource_plan_list.php

---
+++ api/outsource_activate_plan.php
@@ -0,0 +1,209 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header("Content-Type: application/json");
+header("Expires: 0");
+header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
+header("Cache-Control: no-store, no-cache, must-revalidate");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
+date_default_timezone_set('Asia/Kolkata');
+
+include_once('../includes/crud.php');
+
+$db = new Database();
+$db->connect();
+include_once('../includes/custom-functions.php');
+include_once('../includes/functions.php');
+$fn = new functions;
+
+$date = date('Y-m-d');
+
+if (empty($_POST['user_id'])) {
+    $response['success'] = false;
+    $response['message'] = "User Id is Empty";
+    print_r(json_encode($response));
+    return false;
+}
+
+if (empty($_POST['plan_id'])) {
+    $response['success'] = false;
+    $response['message'] = "Plan Id is Empty";
+    print_r(json_encode($response));
+    return false;
+}
+
+$user_id = $db->escapeString($_POST['user_id']);
+$plan_id = $db->escapeString($_POST['plan_id']);
+
+
+
+$sql = "SELECT * FROM users WHERE id = $user_id ";
+$db->sql($sql);
+$user = $db->getResult();
+
+if (empty($user)) {
+    $response['success'] = false;
+    $response['message'] = "User not found";
+    print_r(json_encode($response));
+    return false;
+}
+
+$recharge = $user[0]['recharge'];
+$refer_code = $user[0]['refer_code'];
+$referred_by = $user[0]['referred_by'];
+
+$sql = "SELECT * FROM outsource_plan WHERE id = $plan_id ";
+$db->sql($sql);
+$plan = $db->getResult();
+
+if (empty($plan)) {
+    $response['success'] = false;
+    $response['message'] = "Plan not found";
+    print_r(json_encode($response));
+    return false;
+}
+
+$plan_name = $plan[0]['name'];
+$price = $plan[0]['price'];
+$min_refers = $plan[0]['min_refers'];
+$invite_bonus = $plan[0]['invite_bonus'];
+$per_code_cost = $plan[0]['per_code_cost'];
+$datetime = date('Y-m-d H:i:s');
+
+$sql_check = "SELECT * FROM user_plan WHERE user_id = $user_id AND plan_id = $plan_id";
+$db->sql($sql_check);
+$res_check_user = $db->getResult();
+
+if (!empty($res_check_user)) {
+    echo json_encode([
+        'success' => false,
+        'message' => "You have already activated the plan: $plan_name."
+    ]);
+    return false;
+}
+
+$sql_check = "SELECT * FROM outsource_user_plan WHERE user_id = $user_id AND plan_id = $plan_id";
+$db->sql($sql_check);
+$res_check_user = $db->getResult();
+
+if (!empty($res_check_user)) {
+    echo json_encode([
+        'success' => false,
+        'message' => "You have already activated the plan: $plan_name."
+    ]);
+    return false;
+}
+
+
+if (in_array($plan_id, [1, 2, 4, 6])) {
+    $sql_check_plan_5 = "SELECT * FROM outsource_user_plan WHERE user_id = $user_id AND plan_id = 5";
+    $db->sql($sql_check_plan_5);
+    $res_plan_5 = $db->getResult();
+
+    if (!empty($res_plan_5)) {
+        $sql_update_plan_5 = "UPDATE outsource_user_plan SET claim = 0 WHERE user_id = $user_id AND plan_id = 5";
+        $db->sql($sql_update_plan_5);
+    }
+}
+
+if ($recharge >= $price) {
+    if ($refer_code) {
+        $sql = "SELECT * FROM users WHERE refer_code = '$referred_by'";
+        $db->sql($sql);
+        $res = $db->getResult();
+        $num = $db->numRows($res);
+
+        if ($num == 1) {
+            $r_id = $res[0]['id'];
+            $r_refer_code = $res[0]['refer_code'];
+
+            $sql_check_user_plan = "SELECT * FROM outsource_user_plan WHERE user_id = '$r_id' AND plan_id = 5 AND claim = 1";
+            $db->sql($sql_check_user_plan);
+            $res_check_user_plan = $db->getResult();
+         
+            if (!empty($res_check_user_plan)) {
+
+                $total_cost = 0;
+                $total_referrals = 0;
+
+                if ($plan_id == 1) {
+                    $total_cost = 100;
+                    $total_referrals = 1;
+                } elseif ($plan_id == 2) {
+                    $total_cost = 200;
+                    $total_referrals = 1;
+                } elseif ($plan_id == 4) {
+                    $total_cost = 300;
+                    $total_referrals = 1;
+                } elseif ($plan_id == 6) {
+                    $total_cost = 1000;
+                    $total_referrals = 1;
+                }
+            
+                $sql = "UPDATE users SET bonus_wallet = bonus_wallet + $total_cost ,total_referrals = total_referrals + $total_referrals WHERE refer_code = '$referred_by'";
+                $db->sql($sql);
+            
+                $sql = "INSERT INTO transactions (user_id, amount, datetime, type) VALUES ('$r_id', '$total_cost', '$datetime', 'refer_bonus')";
+                $db->sql($sql);
+            } 
+            else {
+                if ($plan_id == 1 || $plan_id == 2 || $plan_id == 4 ) {
+                    $codes = 2000;
+                    $total_cost = $codes * $per_code_cost;
+            
+                    $sql = "UPDATE users SET bonus_wallet = bonus_wallet + $total_cost, total_referrals = total_referrals + 1 WHERE refer_code = '$referred_by'";
+                    $db->sql($sql);
+            
+                    $sql = "INSERT INTO transactions (user_id, amount, datetime, type) VALUES ('$r_id', '$total_cost', '$datetime', 'refer_bonus')";
+                    $db->sql($sql);
+                }
+                if ($plan_id == 6) {
+                    $cost = 4000;
+                    $total_cost = $cost;
+            
+                    $sql = "UPDATE users SET bonus_wallet = bonus_wallet + $total_cost, total_referrals = total_referrals + 1 WHERE refer_code = '$referred_by'";
+                    $db->sql($sql);
+            
+                    $sql = "INSERT INTO transactions (user_id, amount, datetime, type) VALUES ('$r_id', '$total_cost', '$datetime', 'refer_bonus')";
+                    $db->sql($sql);
+                }
+
+            }
+            
+            if ($plan_id == 5) {
+            $total_cost = 5;
+            $codes = 0;
+
+            $sql = "UPDATE users SET bonus_wallet = bonus_wallet + $total_cost WHERE refer_code = '$referred_by'";
+            $db->sql($sql);
+    
+            $sql = "INSERT INTO transactions (user_id, amount, datetime, type) VALUES ('$r_id', '$total_cost', '$datetime', 'refer_bonus')";
+            $db->sql($sql);
+           }
+         
+        }
+    }
+  
+    $sql = "UPDATE users SET recharge = recharge - $price, total_assets = total_assets + $price, withdrawal_status = 1 WHERE id = $user_id";
+    $db->sql($sql);
+
+    $sql_insert_user_plan = "INSERT INTO outsource_user_plan (user_id, plan_id, joined_date, claim) VALUES ('$user_id', '$plan_id', '$date', 1)";
+    $db->sql($sql_insert_user_plan);
+
+    $sql_insert_transaction = "INSERT INTO transactions (user_id, amount, datetime, type) VALUES ('$user_id', '$price', '$datetime', 'plan_activated')";
+    $db->sql($sql_insert_transaction);
+
+    $sql_user = "SELECT * FROM users WHERE id = $user_id";
+    $db->sql($sql_user);
+    $res_user = $db->getResult();
+
+    $response['success'] = true;
+    $response['message'] = "Outsource Plan started successfully";
+    $response['data'] = $res_user;
+} else {
+    $response['success'] = false;
+    $response['message'] = "Low Recharge Balance. Recharge To Activate The Plan";
+}
+
+print_r(json_encode($response));
+?>
---
+++ api/outsource_plan_list.php
@@ -0,0 +1,103 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header("Content-Type: application/json");
+header("Expires: 0");
+header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
+header("Cache-Control: no-store, no-cache, must-revalidate");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
+
+include_once('../includes/crud.php');
+
+$db = new Database();
+$db->connect();
+
+if (empty($_POST['user_id'])) {
+    $response['success'] = false;
+    $response['message'] = "User ID is Empty";
+    print_r(json_encode($response));
+    return false;
+}
+
+$user_id = $db->escapeString($_POST['user_id']);
+$type = isset($_POST['type']) ? $db->escapeString($_POST['type']) : 'jobs';
+
+$sql = "SELECT * FROM users WHERE id = $user_id";
+$db->sql($sql);
+$user = $db->getResult();
+
+if (empty($user)) {
+    $response['success'] = false;
+    $response['message'] = "User not found";
+    print_r(json_encode($response));
+    return false;
+}
+
+$sql = "SELECT * FROM outsource_plan WHERE type = '$type' ORDER BY price ASC";
+$db->sql($sql);
+$res = $db->getResult();
+$num = $db->numRows($res);
+
+if ($num >= 1) {
+    foreach ($res as $row) {
+        $temp['id'] = $row['id'];
+        $temp['name'] = $row['name'];
+        // Remove all HTML tags except for <br>
+        $temp['description'] = strip_tags_except($row['description'], array('br'));
+        $temp['image'] = DOMAIN_URL . $row['image'];
+        $temp['demo_video'] = $row['demo_video'];
+        $temp['monthly_codes'] = $row['monthly_codes'];
+        $temp['monthly_earnings'] = $row['monthly_earnings'];
+        $temp['per_code_cost'] = $row['per_code_cost'];
+        $temp['price'] = $row['price'];
+        $temp['type'] = $row['type'];
+        $temp['min_refers'] = $row['min_refers'];
+        $temp['num_sync'] = $row['num_sync'];
+        $temp['sub_description'] = $row['sub_description'];
+        $temp['active_link'] = $row['active_link'];
+        
+        $plan_id = $row['id'];
+        $sql_check_plan_1_2_3_6 = "SELECT * FROM outsource_user_plan WHERE user_id = $user_id AND (plan_id = 1 OR plan_id = 2 OR plan_id = 4 OR plan_id = 6)";
+        $db->sql($sql_check_plan_1_2_3_6);
+        $has_plan_1_or_2_or_3_or_6 = $db->numRows() > 0;
+    
+        $sql_check_plan = "SELECT * FROM outsource_user_plan WHERE user_id = $user_id AND plan_id = $plan_id";
+        $db->sql($sql_check_plan);
+        $plan_exists = $db->numRows() > 0;
+    
+        if ($plan_id == 5 && $has_plan_1_or_2_or_3_or_6){
+            $temp['status'] = 2; 
+        } else {
+            $temp['status'] = $plan_exists ? 1 : 0;
+        }
+        
+        $rows[] = $temp;
+    }
+    $response['success'] = true;
+    $response['message'] = "Outsource Plan Details Listed Successfully";
+    $response['data'] = $rows;
+    print_r(json_encode($response));
+} else {
+    $response['success'] = false;
+    $response['message'] = "Outsource Plan not found";
+    print_r(json_encode($response));
+}
+
+function strip_tags_except($string, $exceptions = array()) {
+    foreach ($exceptions as $tag) {
+        $string = str_replace("<$tag>", "#{$tag}#", $string);
+        $string = str_replace("</$tag>", "#/{$tag}#", $string);
+    }
+    // Remove HTML tags and their attributes
+    // Remove \r\n characters
+    $string = str_replace(array("\r", "\n"), '', $string);
+    $string = strip_tags($string);
+    // Decode HTML entities to symbols
+    $string = html_entity_decode($string);
+    foreach ($exceptions as $tag) {
+        $string = str_replace("#{$tag}#", "<$tag>", $string);
+        $string = str_replace("#/{$tag}#", "</$tag>", $string);
+    }
+    return $string;
+}
+?>

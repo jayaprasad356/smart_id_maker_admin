e922056a6011185e551807e54fe808283434c004
Akraja712 <ashokkumar7hitter@gmail.com>
2025-01-19 20:54:19 +0530

    plan_list.php

--- api/plan_list.php
+++ api/plan_list.php
@@ -57,15 +57,15 @@ if ($num >= 1) {
         $temp['active_link'] = $row['active_link'];
         
         $plan_id = $row['id'];
-        $sql_check_plan_1_2_3_6 = "SELECT * FROM user_plan WHERE user_id = $user_id AND (plan_id = 1 OR plan_id = 2 OR plan_id = 4 OR plan_id = 6)";
-        $db->sql($sql_check_plan_1_2_3_6);
-        $has_plan_1_or_2_or_3 = $db->numRows() > 0;
+        $sql_check_plan_1_2_4_6 = "SELECT * FROM user_plan WHERE user_id = $user_id AND (plan_id = 1 OR plan_id = 2 OR plan_id = 4 OR plan_id = 6)";
+        $db->sql($sql_check_plan_1_2_4_6);
+        $has_plan_1_or_2_or_4_or_6 = $db->numRows() > 0;
     
         $sql_check_plan = "SELECT * FROM user_plan WHERE user_id = $user_id AND plan_id = $plan_id";
         $db->sql($sql_check_plan);
         $plan_exists = $db->numRows() > 0;
     
-        if ($plan_id == 5 && $has_plan_1_or_2_or_3_or_6){
+        if ($plan_id == 5 && $has_plan_1_or_2_or_4_or_6){
             $temp['status'] = 2; 
         } else {
             $temp['status'] = $plan_exists ? 1 : 0;
